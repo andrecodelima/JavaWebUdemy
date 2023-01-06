430af2464956a8bc9d80668d661df47300afd8a6
Andre Luis Lima da Silva- Ilha Service Tecnologia <andre.lima@corp.int.gov.br>
2023-01-06 15:17:16 -0300

    new start project

---
+++ coursejavweb/src/main/java/com/udemy/course/coursejavweb/CoursejavwebApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.course.coursejavweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursejavwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursejavwebApplication.class, args);
+	}
+
+}
---
+++ coursejavweb/src/test/java/com/udemy/course/coursejavweb/CoursejavwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.course.coursejavweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursejavwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
