272d0f5da1faf8e9da0f1e951176315580150441
Andre Luis Lima da Silva- Ilha Service Tecnologia <andre.lima@corp.int.gov.br>
2023-01-06 16:29:30 -0300

    public class TestConfig - start

---
+++ coursejavweb/src/main/java/com/udemy/course/coursejavweb/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.udemy.course.coursejavweb.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.udemy.course.coursejavweb.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
