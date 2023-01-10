740ae864fbd8e8220e154a7198b80103e6ef608e
Andre Luis Lima da Silva- Ilha Service Tecnologia <andre.lima@corp.int.gov.br>
2023-01-10 14:52:59 -0300

    User insert - P2

--- coursejavweb/src/main/java/com/udemy/course/coursejavweb/resources/UserResource.java
+++ coursejavweb/src/main/java/com/udemy/course/coursejavweb/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.udemy.course.coursejavweb.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.udemy.course.coursejavweb.entities.User;
 import com.udemy.course.coursejavweb.services.UserService;
@@ -39,9 +41,13 @@ public class UserResource {
 	}
 	
 	@PostMapping // Para inserir dados utilizamos o metodo POST do http
+	
+	//O Método a baixo é para obter 'a resposta 201 do http' e não apenas a 200. A 201 tem informaçoes no cabeçalho. (Estudar)
 	public ResponseEntity<User> insert(@RequestBody User obj){
 		obj = service.insert(obj);
-		return ResponseEntity.ok().body(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 
 		
 	}
